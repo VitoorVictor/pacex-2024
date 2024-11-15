5456291c9aaf63fb951bb628e6b653a37ebe239f
Vitor Victor <vitorvictorblasq@gmail.com>
2024-11-15 20:27:28 -0300

    hotfix

--- src/components/cardMenu/index.tsx
+++ src/components/cardMenu/index.tsx
@@ -26,35 +26,3 @@ export default function CardMenu({ img, name, onAction }: ICardProps) {
     </div>
   );
 }
-
-// export default function CardMenu({ img, name }: ICardProps) {
-//   return (
-//     <div className="flex items-center justify-between w-[36rem] my-2 -translate-x-5">
-//       <div className="translate-x-10 drop-shadow-lg">
-//         <img
-//           src={`/img/${img}.png`}
-//           alt={img}
-//           className="w-full max-w-[9rem] h-auto object-cover"
-//         />
-//       </div>
-//       <div className="bg-[#F9FEFD] py-8 rounded-xl w-full pl-[15%] shadow-xl">
-//         <h1 className="md:text-3xl text-2xl font-black">{name}</h1>
-//       </div>
-//     </div>
-//   );
-// }
-
-{
-  /* <p className="md:text-lg text-gray-500 font-medium">
-  {count <= 1 ? `${count} item` : `${count} itens`}{" "}
-</p> */
-}
-{
-  /* <div>
-  <ChevronRight
-    size={40}
-    className="rounded-full bg-white text-flashy -translate-x-5 shadow-md hover:bg-gray-200 cursor-pointer"
-    onClick={() => onAction()}
-  />
-</div> */
-}
